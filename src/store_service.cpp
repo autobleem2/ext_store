d135ca507c050e450ce8bd2ecfffa766acfd53e9
screemerpl <screemer1@o2.pl>
2026-09-24 20:39:33 +0100

    Format fixes

--- src/store_service.cpp
+++ src/store_service.cpp
@@ -133,7 +133,7 @@ string StoreService::fileNameFor(const StoreFile &file) {
 
 bool StoreService::installable(const StoreItem &item) {
     // temporary for testing - TODO: will be removed later
-    /* 
+    /*
     for (const StoreFile &f : item.files) {
         const string name = ableem::toLowerCopy(fileNameFor(f));
         if (name.size() >= 4 && name.compare(name.size() - 4, 4, ".pkg") == 0)
